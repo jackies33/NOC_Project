a36d2d920734112c8d853ce124ed1a4a0ff04cec
stepanover.tech <gustutest@gmail.com>
2023-10-11 22:10:27 +0800

    Commit

---
+++ NOC/nocproject/custom/CRON/noc/dispatcher/icmp/icmp_main.py
@@ -0,0 +1,122 @@
+
+
+import schedule
+import time
+from db_exec import PSQL_CONN,MONGO,CH
+from pinger import ICMP
+
+''' 
+for daemon setup script
+create  - >> "mcedit /etc/systemd/system/cron_dispetcher_icmp.service"
+copy in cron_dispetcher_icmp.service ->> 
+_______________________________
+
+[Unit]
+Description=Collecting metrics ping for dispetcher to CH
+
+[Service]
+ExecStart=/usr/bin/python3 /opt/cron/noc/dispetcher/icmp/icmp_main.py
+WorkingDirectory=/opt/cron/noc/dispetcher/icmp/
+StandardOutput=file:/var/log/cron_dispetcher/icmp_output.log
+StandardError=file:/var/log/cron_dispetcher/icmp_error.log
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+_________________________________
+
+<<----cron_dispetcher_icmp.service
+
+run next commands -->>>
+_____________________________
+
+sudo systemctl daemon-reload
+sudo systemctl enable cron_dispetcher_icmp.service
+sudo systemctl start cron_dispetcher_icmp.service
+
+______________________________
+
+<<--- run next commands
+
+'''
+
+
+"""
+'id' managed_object_profile for get data, if you need to find out the number of id,
+execute the next query from postgresql - 'select id,name from sa_managedobjectprofile;'
+There you'll need to choose only for check stack status profiles
+"""
+
+n = None
+segments_list = ['p/pe','core']
+my_inventory = []
+"'i' - for correct job scheduler. It's need when you start the service , 'my_inventory' is empty yet, and it's nesseccery to fill it " \
+"'i' - use here like starting point "
+i = 0
+
+class INVENTORY():
+
+    def __init__(self,id_list):
+        self.id_list = id_list
+
+
+
+    def start_job_inventory(self,*args):
+            mongo = MONGO(n, self.id_list)
+            collect = mongo.get_segment_id()
+            id_list1 = ""
+            for i in collect:
+                obj_segment_id = i["id"]
+                id_list1 = id_list1+f"'{obj_segment_id}',"
+            id_list1 = id_list1.rstrip(",")
+            psql = PSQL_CONN(id_list1,n)
+            process = psql.postgre_conn_inv()
+            global my_inventory
+            result = (self.collect_inv(process,collect))
+            return result
+
+
+    def collect_inv(self,inventory,segment_dict):
+            dict_result = []
+            for r in inventory:
+                dict = {}
+                obj_id = r[0]
+                obj_name = r[1]
+                obj_ip = r[2]
+                obj_bi_id = r[3]
+                obj_segment_id = r[4]
+                for d in segment_dict:
+                    if obj_segment_id == d['id']:
+                        obj_segment_name = d['name']
+                        obj_segment_id = d['id']
+                        dict.update({"obj_id":obj_id,"obj_name":obj_name,"obj_ip":obj_ip,
+                                     "obj_segment_name":obj_segment_name,
+                                     "obj_segment_id":obj_segment_id,"obj_bi_id":obj_bi_id})
+                dict_result.append(dict)
+            return dict_result
+
+
+def executer_run():
+    target_list = []
+    icmp_exec = ICMP(my_inventory)
+    results = icmp_exec.executer_icmp()
+    for future in results:
+        target_list.append(future.result())
+    ch = CH(target_list)
+    result = ch.ch_insert()
+
+
+int = INVENTORY(segments_list)
+schedule.every(60).seconds.do(executer_run)
+schedule.every(6).hours.do(int.start_job_inventory)
+
+
+while i == 0:
+    my_inventory = int.start_job_inventory(segments_list)
+    time.sleep(1)
+    i = i+1
+while i == 1:
+    schedule.run_pending()
+    time.sleep(1)
+
+
