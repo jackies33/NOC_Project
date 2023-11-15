e0e9d10b3363ee7d12448b141f1bbb64eb792354
stepanover.tech <gustutest@gmail.com>
2023-11-15 15:28:47 +0800

    Commit

---
+++ NOC/nocproject/custom/noc/CRON/noc/ansible/db_backup/clickhouse_exec.py
@@ -0,0 +1,22 @@
+
+
+
+import subprocess
+from datetime import datetime
+
+
+def exec():
+    try:
+        now = datetime.now()
+        dt_string = now.strftime("Date_%Y-%m-%d_Time_%H-%M-%S")
+        subprocess.run("rm -R /var/lib/clickhouse/shadow/*", shell=True)
+        subprocess.run("clickhouse-backup create", shell=True)
+        subprocess.run(f"mkdir /mnt/sharedfolder_client/Full/clickhouse/noc_backup_{dt_string}", shell=True)
+        subprocess.run(f"cp -R /var/lib/clickhouse/shadow/ /mnt/sharedfolder_client/Full/clickhouse/noc_backup_{dt_string}/", shell=True)
+        return "1"
+    except Exception as e:
+        return "0"
+
+if __name__ == '__main__':
+    executing = exec()
+    print(executing)
