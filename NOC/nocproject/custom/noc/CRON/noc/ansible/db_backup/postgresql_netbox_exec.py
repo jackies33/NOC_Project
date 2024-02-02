31cba31770ddba6aae6aff3c5d4981d7dda9a2da
stepanover.tech <gustutest@gmail.com>
2024-02-02 20:18:01 +0530

    Commit

--- NOC/nocproject/custom/noc/CRON/noc/ansible/db_backup/postgresql_netbox_exec.py
+++ NOC/nocproject/custom/noc/CRON/noc/ansible/db_backup/postgresql_netbox_exec.py
@@ -7,16 +7,15 @@ import os
 
 def exec():
     try:
-        password = 'noc \n'
         now = datetime.now()
         dt_string = now.strftime("Date_%Y-%m-%d_Time_%H-%M-%S")
         subprocess.run("sudo mount 10.50.100.75:/opt/nfs/noc.tech.mosreg.ru /mnt/sharedfolder_client", shell=True)
         time.sleep(2)
-        i = 10
+        i = 6
         time.sleep(2)
-        subprocess.run(f"sudo PGPASSWORD=noc pg_dump -U noc noc > /mnt/sharedfolder_client/Full/postgresql/noc_backup_{dt_string}.sql",shell=True)
+        subprocess.run(f"sudo -u netbox PGPASSWORD=P@ssw0rd pg_dump netbox > /mnt/sharedfolder_client/Full/netbox_postgresql/noc_backup_{dt_string}.sql",shell=True)
         time.sleep(2)
-        filename = f"/mnt/sharedfolder_client/Full/postgresql/noc_backup_{dt_string}.sql"
+        filename = f"/mnt/sharedfolder_client/Full/netbox_postgresql/noc_backup_{dt_string}.sql"
         while i > 0:
             time.sleep(5)
             i = i - 1
@@ -24,6 +23,7 @@ def exec():
                 return '1'
             elif i == 0:
                 return '0'
+
     except Exception as e:
         print(e)
         return "0"
